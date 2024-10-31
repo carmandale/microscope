4be618459d2eb63c54d358045b4020a845d79638
Dale Carman <dale@mrgroovejones.com>
2024-10-31 18:54:43 -0500

    Initial Commit

---
+++ Microscope/MicroscopeApp.swift
@@ -0,0 +1,34 @@
+//
+//  MicroscopeApp.swift
+//  Microscope
+//
+//  Created by Dale Carman on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct MicroscopeApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.progressive), in: .progressive)
+    }
+}
---
+++ MicroscopeTests/MicroscopeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MicroscopeTests.swift
+//  MicroscopeTests
+//
+//  Created by Dale Carman on 10/31/24.
+//
+
+import Testing
+@testable import Microscope
+
+struct MicroscopeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
